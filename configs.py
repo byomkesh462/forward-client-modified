9fc7ef1a813acf3bb03b3158205c5b5140a96310
byomkesh462 <88849299+byomkesh462@users.noreply.github.com>
2022-02-18 15:15:29 +0530

    Added allowed extension

--- configs.py
+++ configs.py
@@ -18,6 +18,7 @@ class Config(object):
     DEFAULT_FILTERS = "video document photo audio text gif forwarded poll sticker"
     FORWARD_FILTERS = list(set(x for x in os.environ.get("FORWARD_FILTERS", DEFAULT_FILTERS).split()))
     BLOCKED_EXTENSIONS = list(set(x for x in os.environ.get("BLOCKED_EXTENSIONS", "").split()))
+    ALLOWED_EXTENSIONS = list(set(x for x in os.environ.get("ALLOWED_EXTENSIONS", "").split()))
     MINIMUM_FILE_SIZE = os.environ.get("MINIMUM_FILE_SIZE", None)
     BLOCK_FILES_WITHOUT_EXTENSIONS = bool(os.environ.get("BLOCK_FILES_WITHOUT_EXTENSIONS", False))
     # Forward as Copy. Value Should be True or False
